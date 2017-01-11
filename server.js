c1338606577be8a47db74d35ad7258acf87796f5
Jeremie CORPINOT <Jeremie CORPINOT>
2017-01-11 09:16:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,12 +21,13 @@ app
 .use('/quotes', require('./routes/quotes'))
 .use('/unsplash', require('./routes/unsplash'));
 
-// if (require.main === module) {
-//   var server = http.createServer(app);
-//   server.listen(process.env.PORT || 3000, function() {
-//     console.log('Listening on %j', server.address());
-//   });
-// }
-http.createServer(app).listen(app.get('port') || 3000, function(){
-  console.log("Express server listening on port: " + app.get('port'));
-});
+if (require.main === module) {
+  var server = http.createServer(app);
+  server.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on %j', server.address());
+    console.log(process.env.PORT);
+  });
+}
+// http.createServer(app).listen(app.get('port') || 3000, function(){
+//   console.log("Express server listening on port: " + app.get('port'));
+// });
